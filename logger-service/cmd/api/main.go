3c1068a783d2697859bb5ea6dc750b7c00ae7c07
Attila, Balázs <ifearnoghosts@gmail.com>
2022-09-13 19:44:00 +0200

    Create main.go
    logger service main pkg

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpc     = "50001"
+)
+
+// package level variable
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+
+	// create a context in order to disconnect
+	ctx, cancle := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancle()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	return c, nil
+}

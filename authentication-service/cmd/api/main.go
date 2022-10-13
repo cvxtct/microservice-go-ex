668357cac0f91eb68cb26981a1e9072bcc721ed0
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-13 10:07:50 +0200

    Update main.go
    call data repository interface in Config struct

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -19,8 +19,9 @@ const webPort = "80"
 var counts int64
 
 type Config struct {
-	DB     *sql.DB
-	Models data.Models
+	// DB     *sql.DB
+	// Models data.Models
+	Repo data.Repository
 }
 
 func main() {
@@ -34,8 +35,8 @@ func main() {
 	// set up config
 
 	app := Config{
-		DB:     conn,
-		Models: data.New(conn),
+		// DB:     conn,
+		// Models: data.New(conn),
 	}
 
 	// define server
@@ -88,4 +89,9 @@ func connectToDB() *sql.DB {
 		time.Sleep(2 * time.Second)
 		continue
 	}
+}
+
+func (app *Config) setupRepo(conn *sql.DB) {
+	db := data.NewPostgresRepository(conn)
+	app.Repo = db
 }

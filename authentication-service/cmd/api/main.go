f81efc0a3adbd5e60f4bd61ebda7aa46518e06c5
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-13 12:18:35 +0200

    Update main.go
    remove tests and interface

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -19,10 +19,8 @@ const webPort = "80"
 var counts int64
 
 type Config struct {
-	// DB     *sql.DB
-	// Models data.Models
-	Repo   data.Repository
-	Client *http.Client
+	DB     *sql.DB
+	Models data.Models
 }
 
 func main() {
@@ -36,9 +34,8 @@ func main() {
 	// set up config
 
 	app := Config{
-		// DB:     conn,
-		// Models: data.New(conn),
-		Client: &http.Client{},
+		DB:     conn,
+		Models: data.New(conn),
 	}
 
 	// define server
@@ -91,9 +88,4 @@ func connectToDB() *sql.DB {
 		time.Sleep(2 * time.Second)
 		continue
 	}
-}
-
-func (app *Config) setupRepo(conn *sql.DB) {
-	db := data.NewPostgresRepository(conn)
-	app.Repo = db
 }
